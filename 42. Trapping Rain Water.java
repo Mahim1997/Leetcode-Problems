28cc3bc91540b0293d180aa06ffc71a51f25b367
Mahim1997 <38731142+Mahim1997@users.noreply.github.com>
2021-03-03 22:37:19 +0600

    Added prefixes & suffixes

--- 42. Trapping Rain Water.java
+++ 42. Trapping Rain Water.java
@@ -13,19 +13,52 @@ class Solution {
         }
         return max;
     }
+    
+    private int trapUsingSuffixAndPrefix(int[] height){
+        int heightCumulative = 0;
+        
+        // Using Prefixes & Suffixes array
+        int num_elements = height.length;
+        
+        int[] prefix_sum = new int[num_elements];
+        int[] suffix_sum = new int[num_elements];
+        
+        // Compute prefixes and suffixes
+        prefix_sum[0] = height[0];
+        for(int i=1; i<num_elements; i++){
+            prefix_sum[i] = Math.max(prefix_sum[i-1], height[i]);
+        }
+        
+        suffix_sum[num_elements - 1] = height[num_elements - 1];
+        for(int i=num_elements-2; i>=0; i--){
+            suffix_sum[i] = Math.max(suffix_sum[i+1], height[i]);
+        }
+        
+        // Compute heights.
+        for(int i=0; i<height.length; i++){
+            int leftMax = prefix_sum[i];
+            int rightMax = suffix_sum[i];
+            int heightBox = Math.min(leftMax, rightMax) - height[i];
+            heightCumulative += heightBox;
+        }
+        
+        return heightCumulative;
+    }
+    
     public int trap(int[] height) {
         if(height.length <= 1) return 0;
         
         // Brute Force. -> O(n^2)
         int heightCumulative = 0;
-        for(int i=0; i<height.length; i++){
-            int leftMax = getMaxLeft(i, height);
-            int rightMax = getMaxRight(i, height);
-            int heightBox = Math.min(leftMax, rightMax) - height[i]; // subtract current height
-            heightCumulative += heightBox;
-        }
+        // for(int i=0; i<height.length; i++){
+        //     int leftMax = getMaxLeft(i, height);
+        //     int rightMax = getMaxRight(i, height);
+        //     int heightBox = Math.min(leftMax, rightMax) - height[i]; // subtract current height
+        //     heightCumulative += heightBox;
+        // }
         
-        
+        // Prefixes & Suffixes -> O(n) Space, O(n) Time Complexity
+        heightCumulative = trapUsingSuffixAndPrefix(height);
         return heightCumulative;
     }
 }
