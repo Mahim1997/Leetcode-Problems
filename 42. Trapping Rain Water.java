18bd961ba7b9b13ff5bb215e8ff3514c49a1faf7
Mahim1997 <38731142+Mahim1997@users.noreply.github.com>
2021-03-06 16:18:56 +0600

    Two pointers approach updated in code

--- 42. Trapping Rain Water.java
+++ 42. Trapping Rain Water.java
@@ -56,14 +56,51 @@ class Solution {
         return heightCumulative;
     }
     
+    private int trapUsing2Pointers(int[] height){
+        int left_ptr = 0;
+        int right_ptr = height.length - 1;
+        int left_max = 0, right_max = 0;
+        int cumulative_height = 0;
+        while(left_ptr < right_ptr){
+            // System.out.println("left_ptr = " + left_ptr + " , right_ptr = " + right_ptr + " , height[l] = " + height[left_ptr] + ", height[r] = " + height[right_ptr] + ", lmax = " + left_max + ", rmax = " + right_max);
+            if(height[left_ptr] < height[right_ptr]){ // Smaller one is taken care of first .. since we want Min(leftMax, rightMax)
+                // This case for assigning height at left_ptr.
+                if(height[left_ptr] > left_max){ 
+                    // Checking if maximum, then simply assign.
+                    left_max = height[left_ptr];
+                }else{ 
+        // Else, add to result i.e. remove left_max (the highest of left and right side)
+                    cumulative_height += (left_max - height[left_ptr]);
+                }
+                left_ptr++; // Incrememnt as left-side was better than right-side
+            }
+            else{ // condition: if(height[left_ptr] < height[right_ptr])
+                // This case for assigning height at right_ptr.
+                if(height[right_ptr] > right_max){ // Same thing as in left_ptr above.
+                    right_max = height[right_ptr];
+                }else{
+                    cumulative_height += (right_max - height[right_ptr]);
+                }
+                right_ptr--;
+            }
+        }
+        return cumulative_height;
+    }
+	
     public int trap(int[] height) {
         if(height.length <= 1) return 0;
+		int heightCumulative;
         
         // Brute Force. -> O(n^2)
         // heightCumulative = trapUsingBruteForce(height);
         
         // Prefixes & Suffixes -> O(n) Space, O(n) Time Complexity
-        heightCumulative = trapUsingSuffixAndPrefix(height);
-        return heightCumulative;
+        // heightCumulative = trapUsingSuffixAndPrefix(height);
+        
+		// Using two pointers approach.
+		// Min of two monotonically increasing functions -> can use 2 pointers approach.
+		heightCumulative = trapUsing2Pointers(height);
+		
+		return heightCumulative;
     }
 }
