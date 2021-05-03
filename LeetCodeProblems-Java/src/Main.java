1d384a401f15228ac685ae78f26d31bbde720a2a
Mahim Mahbub <mahim.mahbub.97@gmail.com>
2021-05-04 02:26:52 +0600

    .

--- LeetCodeProblems-Java/src/Main.java
+++ LeetCodeProblems-Java/src/Main.java
@@ -16,9 +16,18 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        char[][] matrix = {{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
+//        char[][] matrix = {{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
+//
+//        Solution sol = new Solution();
+//        sol.maximalRectangle(matrix);
+        Integer[] arr = {1,2,3,4,5,6,7,8,9};
+        int ans=0;
 
-        Solution sol = new Solution();
-        sol.maximalRectangle(matrix);
+        List<Integer> list = new ArrayList<>(Arrays.asList(arr));
+        System.out.println(list);
+
+        int sum = 0;
+        sum = list.stream().reduce(sum, Integer::sum);
+        System.out.print(sum);
     }
 }
