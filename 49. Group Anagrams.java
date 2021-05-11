b4c609ecb57b8fd64aef4b2664fbc86d48fd2218
Mahim Mahbub <mahim.mahbub.97@gmail.com>
2021-05-11 11:46:24 +0600

    Group anagrams updated

--- 49. Group Anagrams.java
+++ 49. Group Anagrams.java
@@ -9,35 +9,16 @@ class Solution {
         return new String(arr);
     }
     public List<List<String>> groupAnagrams(String[] arr) {
-        // Sort each string.
-        String[] sorted_arr = new String[arr.length];
-        for(int i=0; i<arr.length; i++){
-            sorted_arr[i] = sortString(arr[i]);
-        }
-        
         // Create a map of "KEY" with "indices" -> each clusters/anagrams.
-        Map<String, List<Integer>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
         
-        for(int i=0; i<sorted_arr.length; i++){
-            String s = sorted_arr[i];
-            if(map.containsKey(s) == false){
-                map.put(s, new ArrayList<>()); // insert new array list.
-            }
-            // append this index.
-            List list = map.get(s);
-            list.add(i);
+        for(int i=0; i<arr.length; i++){
+            // String s = sortString(arr[i]); // On the fly, sort each string
+            
+			map.putIfAbsent(s, new ArrayList<>()); // one liner
+            map.get(s).add(arr[i]); // append this string (initial string).
         }
         
-        // take the values of above map, and put into double list.
-        List<List<String>> ans = new ArrayList<>();
-        for(String key: map.keySet()){
-            List<String> temp_list = new ArrayList<>();
-            for(int idx: map.get(key)){
-                temp_list.add(arr[idx]);    
-            }
-            ans.add(temp_list);
-        }
-        
-        return ans;
+        return new ArrayList(map.values()); // works
     }
 }
--- LeetCodeProblems-Java/src/Main.java
+++ LeetCodeProblems-Java/src/Main.java
@@ -16,9 +16,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        char[][] matrix = {{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
+        String s = "mbaocd";
+        s.toCharArray().sort(
 
-        Solution sol = new Solution();
-        sol.maximalRectangle(matrix);
+        );
+        System.out.println(s);
+
     }
 }
