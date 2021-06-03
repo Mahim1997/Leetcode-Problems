8f6dca69dac7f90b0401efa3dfedffe7b437e2b2
Mahim1997 <38731142+Mahim1997@users.noreply.github.com>
2021-06-03 19:50:37 +0600

    VB codes added

---
+++ Visual Basic Codes/WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Const V As String = "Cnt"
+
+    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label1.Click
+        Label1.Text = V
+
+    End Sub
+End Class
