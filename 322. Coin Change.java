55ad722fed5cdcef7bd757510fadb9a3471deb50
Mahim <mahim.mahbub.97@gmail.com>
2021-10-03 16:26:34 +0600

    Top Down & Bottom Up both included

--- 322. Coin Change.java
+++ 322. Coin Change.java
@@ -1,5 +1,48 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        // return bottomUpApproach(coins, amount);
+        return topDownApproach(coins, amount);
+    }
+    
+    private int topDownApproach(int[] coins, int amount){
+        if(amount < 1){
+            return 0;
+        }
+        int[] cache = new int[amount];
+        return recursiveFunction(coins, amount, cache);
+    }
+    
+    private int recursiveFunction(int[] coins, int currentAmount, int[] cache){
+        
+        // Base cases.
+        if(currentAmount < 0){
+            return -1; // stop, exceeded amount.
+        }
+        if(currentAmount == 0){
+            return 0; // stop, found correct amount.
+        }
+        
+        // Check if already solution exists, and return.
+        if(cache[currentAmount - 1] != 0){
+            return cache[currentAmount - 1];
+        }
+        
+        // Check for each coin.
+        int min = Integer.MAX_VALUE;
+        for(int coin: coins){
+            int res = recursiveFunction(coins, currentAmount - coin, cache);
+            if(res >= 0 && res < min){
+                // if the returned result is a better result.
+                min = 1 + res;
+            }
+        }
+        
+        // Update in cache.
+        cache[currentAmount - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+        return cache[currentAmount - 1];
+    }
+    
+    private int bottomUpApproach(int[] coins, int amount){
         int[] dp = new int[amount + 1];
         
         Arrays.sort(coins); // sort coin ordering, to maintain bottom-up DP.
@@ -42,6 +85,5 @@ class Solution {
         // System.out.println("");
         
         return (dp[amount] == Integer.MAX_VALUE) ? -1 : dp[amount];
-        
     }
 }
