c07224bacb1d897a362a84812b704c331c9deac6
Mahim <mahim.mahbub.97@gmail.com>
2021-12-07 01:34:22 +0600

    Organized

--- Visual Basic Codes/WinFormsApp1/WinFormsApp1/Form1.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Form1
-    Private Const V As String = "Cnt"
-
-    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label1.Click
-        Label1.Text = V
-
-    End Sub
-End Class
