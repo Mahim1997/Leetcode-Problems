05ff13b5857faa139f28d5fec54b137eb0e30641
Mahim <mahim.mahbub.97@gmail.com>
2021-12-07 02:49:12 +0600

    updated

--- Leetcode-Premium/Google/2. Add Two Numbers.java
+++ Leetcode-Premium/Google/2. Add Two Numbers.java
@@ -10,6 +10,64 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode node1, ListNode node2) {
+        // return addTwoNumbersNormalCode(node1, node2);
+        return addTwoNumbersCleanCode(node1, node2);
+    }
+    
+    private ListNode addTwoNumbersCleanCode(ListNode node1, ListNode node2){
+        ListNode ans = new ListNode();
+        ListNode head = ans;
+        
+        int carryIn = 0;
+        int sum = 0;
+        
+        // loop over both nodes.
+        while((node1 != null) && (node2 != null)){
+            // sum and carry computations.
+            sum = node1.val + node2.val + carryIn;
+            carryIn = sum/10;
+            sum = sum%10;
+            
+            // insert into answer node.
+            ans.next = new ListNode(sum, null);
+            ans = ans.next;
+            
+            // iterate to next node.
+            node1 = node1.next;
+            node2 = node2.next;
+        }
+        
+        // individually check each node for remaining values
+        while((node1 != null) || (node2 != null)){
+            if(node1 != null){ // use node1.val
+                sum = node1.val + carryIn;
+                node1 = node1.next;
+            }else{  // use node2.val
+                sum = node2.val + carryIn;
+                node2 = node2.next;
+            }
+            
+            // carry and sum computations
+            carryIn = sum/10;
+            sum = sum%10;
+            
+            // insert into answer node.
+            ans.next = new ListNode(sum, null);
+            ans = ans.next;
+        }
+        
+        // final carry check.
+        if(carryIn > 0){
+            // insert into answer node.
+            ans.next = new ListNode(1, null);
+            ans = ans.next;
+        }
+        
+        
+        return head.next;
+    }
+    
+    private ListNode addTwoNumbersNormalCode(ListNode node1, ListNode node2) {
         ListNode ans = new ListNode();
         ListNode head = ans;
         
