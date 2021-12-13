7e1fcf69c16c7941ca56cbdf854e8980d366861e
Mahim <mahim.mahbub.97@gmail.com>
2021-12-14 03:30:52 +0600

    Includes bottom up

--- Leetcode-Premium/DP-Problems/322. Coin Change.java
+++ Leetcode-Premium/DP-Problems/322. Coin Change.java
@@ -1,12 +1,44 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         // sorting ? NOT NEEDED
-        if(amount == 0)
-            return 0;
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int ans = dp(coins, amount, map);
+        // return (ans == Integer.MAX_VALUE) ? -1 : ans;
         
-        Map<Integer, Integer> map = new HashMap<>();
-        int ans = dp(coins, amount, map);
-        return (ans == Integer.MAX_VALUE) ? -1 : ans;
+        return bottomUp(coins, amount);
+    }
+    
+    private int bottomUp(int[] coins, int amount){
+        if(amount == 0){
+            return 0;
+        }
+        Arrays.sort(coins);
+        int[] dp = new int[amount + 1]; // exactly equal to amount can be used.
+        
+        // base cases.
+        for(int currentAmount=1; currentAmount<=amount; currentAmount++){
+            dp[currentAmount] = Integer.MAX_VALUE;
+        }
+        for(int coin: coins){
+            if(coin <= amount){
+                dp[coin] = 1; // only 1 coin is enough
+            }
+        }
+        
+        // recursive cases.
+        for(int i=1; i<=amount; i++){
+            int min = Integer.MAX_VALUE;
+            for(int coin: coins){
+                if(i >= coin){
+                    min = Math.min(min, dp[i - coin]);
+                    if(min != Integer.MAX_VALUE){
+                        dp[i] = min + 1;
+                    }
+                }
+            }
+        }
+        
+        return (dp[amount] == Integer.MAX_VALUE) ? -1 : dp[amount];
     }
     
     private int dp(int[] coins, int currentAmount, Map<Integer, Integer> map){
