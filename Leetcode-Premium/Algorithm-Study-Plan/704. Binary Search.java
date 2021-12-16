ba480c0eba2fcb2c27569dbcc8c844c6b126b53b
Mahim <mahim.mahbub.97@gmail.com>
2021-12-17 01:47:18 +0600

    704. Binary Search

---
+++ Leetcode-Premium/Algorithm-Study-Plan/704. Binary Search.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        
+        while(left <= right){
+            mid = left + (right - left)/2; // avoid overflow
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(target < nums[mid]){ // need to check the left side
+                right = mid - 1;
+            }else{ // need to check right side
+                left = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+}
+
+/*
+[-1,0,5,9,12]
+5
+[-1,0,5,9,12]
+-1
+[-1,0,5,9,12]
+12
+[-1,0,5,9,12]
+0
+[-1,0,5,9,12,13]
+0
+[-1,0,5,9,12,13]
+13
+[-1,0,5,9,12,13]
+-1
+*/
