e4b492fcf202f189ed80ca6beace09a1a1b36459
Mahim <mahim.mahbub.97@gmail.com>
2021-12-27 16:49:13 +0600

    .

--- Leetcode-Premium/RANDOM/1137. N-th Tribonacci Number.cpp
+++ Leetcode-Premium/RANDOM/1137. N-th Tribonacci Number.cpp
@@ -32,6 +32,25 @@ public:
         return ans;
     }
     
+    int bottomUp(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 1;
+        
+        int dp[n + 1];
+        // Base case
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        
+        // Recursion -> Iteration
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        
+        return dp[n];
+    }
+	
     int bottomUpConstantSpace(int n){
         if(n == 0) return 0;
         if(n == 1) return 1;
@@ -52,26 +71,6 @@ public:
         }
         
         return ans;
-    }
-    
-    int bottomUp(int n){
-        int dp[n + 1];
-        
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        if(n == 2) return 1;
-        
-        // Base case
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
-        
-        // Recursion -> Iteration
-        for(int i=3; i<=n; i++){
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-        }
-        
-        return dp[n];
     }
     
 };
