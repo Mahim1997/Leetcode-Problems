3c8028ac2d7ac4df66b705b6ae7cff7914de8f92
Mahim <mahim.mahbub.97@gmail.com>
2021-12-27 21:39:57 +0600

    198. House Robber updated

--- Leetcode-Premium/DP-Problems/198. House Robber.java
+++ Leetcode-Premium/DP-Problems/198. House Robber.java
@@ -1,14 +1,52 @@
 class Solution {
     public int rob(int[] nums) {
-        Map<Integer, Integer> cache = new HashMap<>();
-        return dp(nums, nums.length - 1, cache);
+        // Map<Integer, Integer> cache = new HashMap<>();
+        // return dp(nums, nums.length - 1, cache);
+    
+        return bottomUp(nums);
+    }
+
+    private int bottomUpConstantSpace(int[] nums){
+        int n = nums.length;
+        if(n == 1)
+            return nums[0];
+        
+        int twoPrev = nums[0];
+        int onePrev = Math.max(nums[0], nums[1]);
+        if(n == 2)
+            return onePrev;
+        
+        int ans = 0;
+        for(int i=2; i<n; i++){
+            ans = Math.max(onePrev, twoPrev);
+            twoPrev = onePrev;
+            onePrev = ans;
+        }
+        
+        return ans;
     }
     
-    private void printMap(Map<Integer, Integer> map){
-        for(int key: map.keySet()){
-            System.out.println("Key: " + key + ", val = " + map.get(key));
+    private int bottomUp(int[] nums){
+        int n = nums.length;
+        int[] dp = new int[n];        
+        // base cases
+        if(nums.length == 1){
+            return nums[0];
         }
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        
+        // iteration
+        for(int i=2; i<n; i++){
+            // int numsIdx = i - 1; // nums is from 0 indexing
+            int robIdxHouse = dp[i - 2] + nums[i];
+            int dontRobIdxHouse = dp[i - 1] + 0;
+            dp[i] = Math.max(robIdxHouse, dontRobIdxHouse);
+        }
+        
+        return dp[n - 1];
     }
+    
     
     private int dp(int[] nums, int idx, Map<Integer, Integer> cache){
         // base cases.
@@ -34,5 +72,11 @@ class Solution {
         // printMap(cache);
         
         return cache.get(idx);
+    }
+
+    private void printMap(Map<Integer, Integer> map){
+        for(int key: map.keySet()){
+            System.out.println("Key: " + key + ", val = " + map.get(key));
+        }
     }
 }
