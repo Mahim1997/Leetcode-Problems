382d445b5780092b49f753e7dc583a5564308b2b
Mahim <mahim.mahbub.97@gmail.com>
2022-02-14 17:23:02 +0600

    Update 104. Maximum Depth of Binary Tree.java

--- Leetcode-Premium/RANDOM/104. Maximum Depth of Binary Tree.java
+++ Leetcode-Premium/RANDOM/104. Maximum Depth of Binary Tree.java
@@ -15,30 +15,40 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
+        // Recursive approach
+        // if(root == null)
+        //     return 0;
+        // return 1 + Math.max(
+        //     maxDepth(root.left),
+        //     maxDepth(root.right)
+        // );
+        
+        // Using BFS
+        return usingBFS(root);
+    }
+    
+    private int usingBFS(TreeNode root){
+        // edge case
         if(root == null)
             return 0;
         
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-        
-        /*TreeNode node = root;
+        // Binary Tree -> no need for visited set
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(node);
-        int level = 0;
-        
-        while(queue.isEmpty() == false){
+        queue.add(root);
+        int distance = 0;
+        while(!queue.isEmpty()){
             int qSize = queue.size();
             for(int i=0; i<qSize; i++){
-                node = queue.remove();
-                if(node == null)
-                    continue;
+                TreeNode node = queue.remove();
                 if(node.left != null)
                     queue.add(node.left);
+                
                 if(node.right != null)
                     queue.add(node.right);
             }
-            level++;
+            distance++;
         }
-        return level;
-        */
+        return distance;
     }
 }
+
